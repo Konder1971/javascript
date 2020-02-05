3585ee440fbbdcf24d0b144ea8c6d91905249b53
Konder1971 <konder1971@mail.ru>
2020-02-05 15:58:54 +0200

    е

---
+++ JS-CODIM/js/timer.js
@@ -0,0 +1,54 @@
+// https://learn.javascript.ru/while-for
+// http://www.cyberforum.ru/javascript/thread565601.html   Бесконечный цикл
+/*
+setTimeout - функция которая откладывает выполнения определенной части кода, на указанное время. Выполняется однократно.
+setInterval -функция которая запускает выполнение части кода через указанный промежуток времени. Выполняется условно бесконечно.
+clearTimeout( идентификатор );
+clearInterval( идентификатор );
+*/
+
+
+let i = 0;
+let interval = 0;
+let launch = document.querySelector('.launch');
+let stop = document.querySelector('.stop');
+let proceed = document.querySelector('.proceed');
+let back = document.querySelector('.back');
+let discharge = document.querySelector('.discharge');
+
+function countdown() {
+  document.querySelector('.timer').innerHTML += i + ' ';
+  i++;
+}
+
+function reduce() {
+  document.querySelector('.timer').innerHTML += i + ' ';
+  i--;
+}
+
+launch.onclick = function() {
+  launch.disabled = true;
+  interval = setInterval(countdown, 100);
+}
+   
+stop.onclick = function() {
+  clearInterval(interval);
+}
+
+proceed.onclick = function() {
+  interval = setInterval(countdown, 100);
+}
+
+
+back.onclick = function() {
+  interval = setInterval(reduce, 100);
+}
+
+
+discharge.onclick = function() {
+  clearInterval(interval);
+  document.querySelector('.timer').innerHTML = '';
+  i = 0;
+  interval = 0;
+  launch.disabled = false;
+}
