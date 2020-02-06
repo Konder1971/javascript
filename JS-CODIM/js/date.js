db3cc600b18f05a607b3deda846a1762eb6d5afa
Konder1971 <konder1971@mail.ru>
2020-02-06 18:14:11 +0200

    s

--- JS-CODIM/js/date.js
+++ JS-CODIM/js/date.js
@@ -1,10 +1,18 @@
+// https://www.w3schools.com/jsref/jsref_obj_date.asp
+
+// выведем текущую дату
+let mydate = new Date();
+document.querySelector('.mydate').innerHTML = mydate;
+document.querySelector('.year').innerHTML = mydate.getFullYear();
+document.querySelector('.month').innerHTML = mydate.getMonth()+1;
+
 // выведем текущее время
 myTime();
-let time = setInterval(myTime, 1000);
+let mytime = setInterval(myTime, 1000);
 function myTime() {
   let t = new Date();
   let td = t.toLocaleTimeString();
-  document.querySelector('.time').innerHTML = td;
+  document.querySelector('.mytime').innerHTML = td;
 };
 
 // Вы находитесь на странице ... секунд
