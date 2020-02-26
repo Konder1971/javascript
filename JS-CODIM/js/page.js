a64f51fff83dc9a8ba5bda07d28f4127ab85b8d9
Konder1971 <konder1971@mail.ru>
2020-02-26 15:05:11 +0200

    erwgtr

--- JS-CODIM/js/page.js
+++ JS-CODIM/js/page.js
@@ -1,123 +1,96 @@
-
 let number = 0;
 let min = 0;
 let max = 10;
 function randomNumberGeneration(min, max) {
-  return number = Math.floor(min + Math.random() * (max + 1 - min));
+  return (number = Math.floor(min + Math.random() * (max + 1 - min)));
 }
 randomNumberGeneration(min, max);
-document.getElementById('number').innerHTML = 'Генерация случайного чисела от 0 до 10 = ' + number + '<hr />';
-// ***************************************   *************************************** //
+document.getElementById("number").innerHTML =
+  "Генерация случайного чисела от 0 до 10 = " + number;
 
+const number3 = number ** 3; // возвели в куб
+let nb3 = document.createElement('span');
+nb3.innerHTML += ' ' + number3;
+document.querySelector('.number3').appendChild(nb3);
 
-var select = document.querySelector('select');
-var para = document.querySelector('p.p-select');
+var select = document.querySelector("select");
+var para = document.querySelector("p.p-select");
 
-select.addEventListener('change', setWeather);
+select.addEventListener("change", setWeather);
 
 function setWeather() {
   var choice = select.value;
-  if (choice === 'sunny') {
-    para.textContent = 'Сегодня хорошо и солнечно. Носите шорты! Идите на пляж, или в парк, и купите мороженое.';
-  } else if (choice === 'rainy') {
-    para.textContent = 'Дождь падает за окном; возьмите плащ и зонт, и не находитесь слишком долго на улице.';
-  } else if (choice === 'snowing') {
-    para.textContent = 'Снег падает - морозно! Лучше всего посидеть с чашкой горячего шоколада или слепить снеговика.';
-  } else if (choice === 'overcast') {
-    para.textContent = 'Дождя нет, но небо серое и мрачное; он все может измениться в любую минуту, поэтому на всякий случай возьмите дождевик.';
+  if (choice === "sunny") {
+    para.textContent =
+      "Сегодня хорошо и солнечно. Носите шорты! Идите на пляж, или в парк, и купите мороженое.";
+  } else if (choice === "rainy") {
+    para.textContent =
+      "Дождь падает за окном; возьмите плащ и зонт, и не находитесь слишком долго на улице.";
+  } else if (choice === "snowing") {
+    para.textContent =
+      "Снег падает - морозно! Лучше всего посидеть с чашкой горячего шоколада или слепить снеговика.";
+  } else if (choice === "overcast") {
+    para.textContent =
+      "Дождя нет, но небо серое и мрачное; он все может измениться в любую минуту, поэтому на всякий случай возьмите дождевик.";
   } else {
-    para.textContent = '';
+    para.textContent = "";
   }
 }
 
-var cats = ['Билл', 'Макс', 'Пикси', 'Алиса', 'Жасмин'];
-var info = 'Моих кошек зовут: ';
-var para2 = document.querySelector('div.cats');
+var cats = ["Билл", "Макс", "Пикси", "Алиса", "Жасмин"];
+var info = "Моих кошек зовут: ";
+var para2 = document.querySelector("div.cats");
 for (var i = 0; i < cats.length; i++) {
   if (i === cats.length - 1) {
-    info += 'и ' + cats[i] + '.';
+    info += "и " + cats[i] + ".";
   } else {
-    info += cats[i] + ', ';
+    info += cats[i] + ", ";
   }
 }
 para2.textContent = info;
 
-
-
-// ************************************************************************** //
-// // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Classes/Class_fields
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Classes/Class_fields
 // https://www.youtube.com/watch?v=dB8EGjgEu0s
 
-let privet = document.querySelector('.privet')
-/*
-function Person(name, family, age) {
-  this.name = name
-  this.family = family
-  this.age = age
-
-  Person.count  += 1
-}
-Person.count = 0
-Person.prototype.sayHello = function sayHello () {
-  privet.innerHTML += `Здрасте! Я ${this.name} ${this.family}. Мне ${this.age} лет.<br />`
-}
-Person.prototype.goToHome = function goToHome () {
-  privet.innerHTML += `Персонаж ${this.name} ${this.family} - Самоликвидировался.<br />`
-}
-const person1 =  new Person('Васисуалий', 'Лоханкин', 24)
-const person2 =  new Person('Игнатий', 'Иванопуло', 18)
-const person3 =  new Person('Калистрат', 'Свиридонов', 32)
-console.log('Количество персон = ' + Person.count)
-person1.sayHello()
-person1.goToHome()
-person2.sayHello()
-person2.goToHome()
-*/
-
-
-
+let privet = document.querySelector(".privet");
 class Person {
-  constructor (name, family, age) {
-    this.name = name
-    this.family = family
-    this.age = age
-    Person.count  += 1
+  constructor(name, family, age) {
+    this.name = name;
+    this.family = family;
+    this.age = age;
+    Person.count += 1;
   }
-  sayHello () {
-    privet.innerHTML += `Здрасте! Я ${this.name} ${this.family}. Мне ${this.age} лет.<br />`
+  sayHello() {
+    privet.innerHTML += `Здрасте! Я ${this.name} ${this.family}. Мне ${this.age} лет.<br />`;
   }
-  goToHome () {
-    privet.innerHTML += `Персонаж ${this.name} ${this.family} - Самоликвидировался.<br />`
+  goToHome() {
+    privet.innerHTML += `Персонаж ${this.name} ${this.family} - Самоликвидировался.<br />`;
   }
-  static howMach () {
-    console.log('Всего Количество Персон = ' + Person.count)
+  static howMach() {
+    console.log("Всего Количество Персон = " + Person.count);
   }
-
 }
 
-Person.count = 0
+Person.count = 0;
 
-const person1 =  new Person('Васисуалий', 'Лоханкин', 24)
-const person2 =  new Person('Игнатий', 'Иванопуло', 18)
-const person3 =  new Person('Калистрат', 'Свиридонов', 32)
-const person4 =  new Person('Иннокентий', 'Зойцман', 46)
+const person1 = new Person("Васисуалий", "Лоханкин", 24);
+const person2 = new Person("Игнатий", "Иванопуло", 18);
+const person3 = new Person("Калистрат", "Свиридонов", 32);
+const person4 = new Person("Иннокентий", "Зойцман", 46);
 
-console.log('Всего Количество Персон = ' + Person.count)
+console.log("Всего Количество Персон = " + Person.count);
 
-person1.sayHello()
-person1.goToHome()
-person2.sayHello()
-person2.goToHome()
+person1.sayHello();
+person1.goToHome();
+person2.sayHello();
+person2.goToHome();
 
-
-
-// ******************************************************* //
 // Наследование
-let animal = document.querySelector('.animal')
+let animal = document.querySelector(".animal");
 class Animal {
   constructor(name) {
-    this.name = name
-    this.sound = ''
+    this.name = name;
+    this.sound = "";
   }
   voice() {
     console.log(this.sound);
@@ -125,23 +98,23 @@ class Animal {
 }
 
 class Dog extends Animal {
-  constructor (name) {
-    super(name) //  передали данные на уровень выше
-    this.sound = 'Гав'
+  constructor(name) {
+    super(name); //  передали данные на уровень выше
+    this.sound = "Гав";
   }
   voice() {
-    console.log('Собака Гавкает');
+    console.log("Собака Гавкает");
   }
 }
 
 class Cat extends Animal {
   constructor() {
-    super(name) //  передали данные на уровень выше
-    this.sound = 'Мяу'
+    super(name); //  передали данные на уровень выше
+    this.sound = "Мяу";
   }
 }
 
-const dog = new Dog ('Бобик')
-const cat = new Cat ('Мурка')
-dog.voice()
-cat.voice()
+const dog = new Dog("Бобик");
+const cat = new Cat("Мурка");
+dog.voice();
+cat.voice();
