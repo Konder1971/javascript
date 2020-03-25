6d67479937f1f0e5cada421bc753f7538cd5d01b
Konder1971 <konder1971@mail.ru>
2020-03-25 21:46:48 +0200

    dsfd

--- GSAP/js/bepl.js
+++ GSAP/js/bepl.js
@@ -6,7 +6,7 @@ const boxTre = document.querySelector('.tre')
 
 const btnUno = document.querySelector('.button-uno')
 const btnDuo = document.querySelector('.button-duo')
-// const btnTre = document.querySelector('.button-tre')
+const btnTre = document.querySelector('.button-tre')
 
 function simpleAnima() {
   const simpleAnimation = new gsap.timeline({ yoyo: true, repeat: 1 })
@@ -25,22 +25,77 @@ function simpleAnima() {
 btnUno.addEventListener('click', simpleAnima, false)
 
 function bez() {
-  const tl = new gsap.timeline({ yoyo: true, repeat: 1})
-  tl.to(boxUno, 20, {
+  const tl = new gsap.timeline({ yoyo: true, repeat: 1 })
+  tl.to(boxUno, {
+    duration: 20,
     motionPath: {
       path: [
         { x: 0, y: 0 },
         { x: 200, y: -100 },
         { x: 300, y: 200 },
         { x: 500, y: 500 },
-        { x: 800, y: -150 },
-        { x: 1000, y: 150 },
         { x: 1600, y: 650 }
       ],
-      //type: 'cubic'
+      // type: 'cubic'
       autoRotate: true
     },
     ease: 'power2.out'
   })
 }
 btnDuo.addEventListener('click', bez, false)
+
+function complexTre() {
+  const tlComplex = gsap.timeline({ yoyo: true, repeat: 1 })
+  tlComplex.to(
+    boxUno,
+    {
+      duration: 20,
+      motionPath: {
+        path: [
+          { x: 0, y: 0 },
+          { x: 100, y: -100 },
+          { x: 300, y: 0 },
+          { x: 500, y: 500 },
+          { x: 1600, y: 250 }
+        ]
+      },
+      ease: 'power2.out'
+    },
+    0
+  )
+  tlComplex.to(
+    boxDuo,
+    {
+      duration: 20,
+      motionPath: {
+        path: [
+          { x: 0, y: 0 },
+          { x: 100, y: 100 },
+          { x: 300, y: 240 },
+          { x: 500, y: 0 },
+          { x: 1600, y: 350 }
+        ]
+      },
+      ease: 'power2.out'
+    },
+    0
+  )
+  tlComplex.to(
+    boxTre,
+    {
+      duration: 20,
+      motionPath: {
+        path: [
+          { x: 0, y: 0 },
+          { x: 50, y: 100 },
+          { x: 300, y: -200 },
+          { x: 500, y: 0 },
+          { x: 1600, y: 50 }
+        ]
+      },
+      ease: 'power2.out'
+    },
+    0
+  )
+}
+btnTre.addEventListener('click', complexTre, false)
